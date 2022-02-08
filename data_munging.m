c34c5543245daaba2ab03b8263a1afa31c06961b
olanrewajufarooq <olanrewajufarooq@yahoo.com>
2022-02-08 12:20:21 +0100

    Update

---
+++ data_munging.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = data_munging(inputArg1,inputArg2)
+%DATA_MUNGING Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
